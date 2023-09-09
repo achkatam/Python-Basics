57d096be9e8c18cebdc7d406bc702a9a4207b0dd
achkatam <agmatevv@gmail.com>
2023-09-09 11:21:15 -0400

    added math operators

---
+++ arithmetic_operations.py
@@ -0,0 +1,28 @@
+print(10 + 2)
+
+print(10 - 2)
+
+print(10 * 2)
+
+# Returns floating point number
+print(10 / 2) # 5.0
+
+# Returns integer
+print(10 // 2)  # 5
+
+# Returns the remainder
+print(10 % 2)
+print(10 % 3)
+
+# Power a number
+print(10 ** 3)
+   
+# Augmented assignment operator
+x = 10
+x += 3
+print(x)
+
+# Decreasing operator
+x = 10
+x -= 3
+print(x)
---
+++ operator_precedence.py
@@ -0,0 +1,6 @@
+x = 10 + 3 * 2
+print(x) # 16
+
+# Using parentheses
+x = (10 + 3) * 2
+print(x) # 26
