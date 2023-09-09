346283d8a33a32a9d0dc65dbca20ffd324fc051e
achkatam <agmatevv@gmail.com>
2023-09-09 11:33:39 -0400

    added some comparison operators and if statements

---
+++ comparison_operators.py
@@ -0,0 +1,25 @@
+# BOOLEAN COMPARISON OPERATORS
+
+# Greaer than: >
+x = 3 > 2
+print(x) # True
+
+# Less than: <
+x = 3 < 2
+print(x) # False
+
+# Greater than or equal to: >=
+x = 3 >= 2
+print(x) # True
+
+# Less than or equal to: <=
+x = 3 <= 2
+print(x) # False
+
+# Equal to: ==
+x = 3 == 2
+print(x) # False
+
+# Not equal to: !=
+x = 3 != 2
+print(x) # True
---
+++ if_statements.py
@@ -0,0 +1,8 @@
+temp = 29
+
+if temp > 30:
+    print("It's a hot day")
+elif  temp >= 28 and temp <= 30:
+    print("It's not a hot day")
+else:
+    print("It's a chilly day")
