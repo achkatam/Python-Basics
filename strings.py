a16539685d83def7f989a82669e7ed1c163056a7
achkatam <agmatevv@gmail.com>
2023-09-09 11:04:50 -0400

    basic commands for strings - capitalization, lowering, replacing, finding a char and so on

---
+++ strings.py
@@ -0,0 +1,16 @@
+course = "Python for Beginners"
+print(course.upper())
+print(course)
+
+print(course.lower())
+
+# Return index of a character
+print(course.find("n"))
+
+# Replace a character
+print(course.replace("for", "4"))
+
+# Return a boolean value
+print("Python" in course) # True
+
+print("python" in course) # False
