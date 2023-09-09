003e775f8b70b4c455a2ea9977ebdcf4ea6ead22
achkatam <agmatevv@gmail.com>
2023-09-09 12:08:15 -0400

    while loops and lists added

---
+++ lists.py
@@ -0,0 +1,14 @@
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+print(names)
+
+# Accessing elements in a list
+print(names[3])
+
+# Negative index representing the last element in the list
+print(names[-1])
+
+names[0] = 'Jon'
+print(names[0])
+
+# List slicing excludes the last index
+print(names[2:4])
---
+++ while_loops.py
@@ -0,0 +1,7 @@
+i = 1
+while i <= 100:
+    print(i)
+    print("Let's increment by 1")
+    if (i == 100):
+        print("We're done!")
+    i += 1
