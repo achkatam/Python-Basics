30a4b3df59061cb5d006aba908ae362dfc807b3b
achkatam <agmatevv@gmail.com>
2023-09-11 09:04:25 -0400

    added more simple code

---
+++ functions.py
@@ -0,0 +1,15 @@
+# Defining functionn with def
+
+def greet_user():
+    print("Hi there")
+    print("Welcome aboard")
+
+
+def ask_user():
+    name = input("What is your name? ")
+    print("Hello " + name)
+
+print("Start")
+greet_user()
+ask_user()
+print("Done")
---
+++ parameters.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome aboard!")
+
+
+print("Start")
+greet_user(first_name="John", last_name="Hopkins")
+greet_user("Mary", "Hopkins")
+print("Done")
